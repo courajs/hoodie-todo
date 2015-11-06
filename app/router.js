d1d6e81331cedaa47675f2843b3c2719e5d810cb
Aaron Sikes <aaron@sikes.io>
2015-11-06 12:19:04 -0500

    Add simple todos index page

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('todos', function(){
+    this.route('show', {path: ':todo_id'});
+  });
 });
 
 export default Router;
